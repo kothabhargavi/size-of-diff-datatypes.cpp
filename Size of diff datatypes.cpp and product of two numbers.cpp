40596aee26d4b0e5be44b684465e9826e03a8343
kothabhargavi <68498062+kothabhargavi@users.noreply.github.com>
2020-08-29 12:48:49 +0530

    To find the size of datatypes and product of two numbers

--- Size of diff datatypes.cpp and product of two numbers.cpp
+++ Size of diff datatypes.cpp and product of two numbers.cpp
@@ -10,3 +10,17 @@ int main()
 
     return 0;
 }
+And 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double firstNumber, secondNumber, productOfTwoNumbers;
+    cout << "Enter two numbers: ";
+    cin >> firstNumber >> secondNumber;
+    productOfTwoNumbers = firstNumber * secondNumber;  
+    cout << "Product = " << productOfTwoNumbers;    
+    return 0;
+}
